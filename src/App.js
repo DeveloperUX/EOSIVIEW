361a24b34d0f70db60204b8a396e0f836032e87b
Moustafa Ismael <mismael7@gmail.com>
2020-01-26 16:43:13 +0200

    new blocks take time to fetch, lets instead show each new fetched block as it rolls in

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ const App = () => {
   const [loadingRecent, setLoadingRecent] = useState(false);
   const [lastBlockNum, setLastBlockNum] = useState(0);
 
+  const MAX_NUM_BLOCKS_TO_SHOW = 10;
+
 
   const loadMoreBlocks = async () => {
     const newBlocks = [];
@@ -21,27 +23,35 @@ const App = () => {
     // Load all blocks since the last fetch
     const numBlocksBehind = head_block_num - lastBlockNum;
     // Max 10 blocks at a time
-    const startingBlock = head_block_num - Math.min(10, numBlocksBehind);
+    const startingBlock = head_block_num - Math.min(MAX_NUM_BLOCKS_TO_SHOW, numBlocksBehind);
     // Load one block at a time starting from the most recent block fetched
     for (let blockNum = startingBlock; blockNum < head_block_num; blockNum ++) {
-      const block = await rpc.get_block(blockNum);
-      newBlocks.push(block);
+      // note fetching blocks can be slow... maybe fetch them one at a time
+      const newBlock = await rpc.get_block(blockNum);
+      // Show the new blocks as they come in
+      setCurBlocks(oldBlocks => {
+        if (oldBlocks.length >= MAX_NUM_BLOCKS_TO_SHOW) {
+          let [dropFirst, ...blocks] = oldBlocks;
+          return [...blocks, newBlock];
+        }
+        return [...oldBlocks, newBlock]
+      });
+      setLastBlockNum(head_block_num);
     }
 
-    setCurBlocks(newBlocks);
-    setLastBlockNum(head_block_num);
     setLoadingRecent(false);
   }
 
   return (
     <main className={style.layout}>
-      <button onClick={loadMoreBlocks}>Load More</button>
+      <button onClick={loadMoreBlocks} disabled={loadingRecent}>Load More</button>
       <table>
         <thead>
           <tr>
-          <th>Block Hash</th>
-          <th>Timestamp</th>
-          <th>Actions</th>
+            <th>Block Hash</th>
+            <th>Timestamp</th>
+            <th>Actions</th>
+            <th>block_num</th>
           </tr>
         </thead>
         <tbody>
@@ -51,6 +61,7 @@ const App = () => {
               <td>{block.id}</td>
               <td>{block.timestamp}</td>
               <td>{block.actions}</td>
+              <td>{block.block_num}</td>
             </tr>
           ))
         }
