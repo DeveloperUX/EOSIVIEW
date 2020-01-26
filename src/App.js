c9dbd46a3f454697a827ca4a104019c69b2e533c
Moustafa Ismael <mismael7@gmail.com>
2020-01-26 14:28:24 +0200

    fetch the latest blocks by keeping track of the last block number fetched

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Api, JsonRpc, RpcError } from 'eosjs';
+import style from './app.module.css'
 
-function App() {
+const App = () => {
+
+  const remoteBlockProducer = 'https://api.eosnewyork.io';
+  const rpc = new JsonRpc(remoteBlockProducer, { fetch });
+
+  const [curBlocks, setCurBlocks] = useState([]);
+  const [loadingRecent, setLoadingRecent] = useState(false);
+  const [lastBlockNum, setLastBlockNum] = useState(0);
+
+
+  const loadMoreBlocks = async () => {
+    const newBlocks = [];
+    setLoadingRecent(true);
+    // fetch the last block number
+    const { head_block_num } = await rpc.get_info();
+    // how many blocks should we load?
+    // Load all blocks since the last fetch
+    const numBlocksBehind = head_block_num - lastBlockNum;
+    // Max 10 blocks at a time
+    const startingBlock = head_block_num - Math.min(10, numBlocksBehind);
+    // Load one block at a time starting from the most recent block fetched
+    for (let blockNum = startingBlock; blockNum < head_block_num; blockNum ++) {
+      const block = await rpc.get_block(blockNum);
+      newBlocks.push(block);
+    }
+
+    setCurBlocks(newBlocks);
+    setLastBlockNum(head_block_num);
+    setLoadingRecent(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={style.layout}>
+      <button onClick={loadMoreBlocks}>Load More</button>
+      <table>
+        <thead>
+          <tr>
+          <th>Block Hash</th>
+          <th>Timestamp</th>
+          <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          curBlocks.map( (block, i) => (
+            <tr key={i}>
+              <td>{block.id}</td>
+              <td>{block.timestamp}</td>
+              <td>{block.actions}</td>
+            </tr>
+          ))
+        }
+        </tbody>
+      </table>
+    </main>
   );
 }
 
