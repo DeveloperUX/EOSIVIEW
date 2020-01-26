6362437f5ea2627e306cad15c0c5a972c4abc8ec
Moustafa Ismael <mismael7@gmail.com>
2020-01-26 20:30:09 +0200

    add a function to count the number of actions in a block

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { Api, JsonRpc, RpcError } from 'eosjs';
 import style from './app.module.css'
 
+const getActionsCount = (block) => {
+  let actionsCount = 0;
+  // make sure we default this to an empty array in case it's undefined
+  const { transactions = [] } = block;
+  for (let transaction of transactions) {
+    const { trx = {} } = transaction;
+    // note: some trx are string IDs while some are full objects
+    if (typeof trx === 'object') {
+      // note: Assuming we need to count both actions and context_free_actions
+      // which are quote: "actions that do not depend upon the blockchain state to perform validation"
+      // temp1.transactions[4].trx.transaction.actions
+      const { transaction = {} } = trx;
+      const { context_free_actions = [], actions = [] } = transaction;
+      actionsCount += context_free_actions.length + actions.length;
+    }
+  }
+  return actionsCount;
+};
+
 const App = () => {
 
   const remoteBlockProducer = 'https://api.eosnewyork.io';
@@ -15,7 +34,6 @@ const App = () => {
 
 
   const loadMoreBlocks = async () => {
-    const newBlocks = [];
     setLoadingRecent(true);
     // fetch the last block number
     const { head_block_num } = await rpc.get_info();
@@ -25,9 +43,15 @@ const App = () => {
     // Max 10 blocks at a time
     const startingBlock = head_block_num - Math.min(MAX_NUM_BLOCKS_TO_SHOW, numBlocksBehind);
     // Load one block at a time starting from the most recent block fetched
-    for (let blockNum = startingBlock; blockNum < head_block_num; blockNum ++) {
+    for (let blockNum = startingBlock; blockNum < head_block_num; blockNum++) {
       // note fetching blocks can be slow... maybe fetch them one at a time
-      const newBlock = await rpc.get_block(blockNum);
+      let newBlock = await rpc.get_block(blockNum);
+      // Add the total count of actions here,
+      // calculating num of actions here will save us from calling it on every render
+      newBlock = {
+        ...newBlock,
+        actionsCount: getActionsCount(newBlock)
+      }
       // Show the new blocks as they come in
       setCurBlocks(oldBlocks => {
         if (oldBlocks.length >= MAX_NUM_BLOCKS_TO_SHOW) {
@@ -38,7 +62,6 @@ const App = () => {
       });
       setLastBlockNum(head_block_num);
     }
-
     setLoadingRecent(false);
   }
 
@@ -60,7 +83,7 @@ const App = () => {
             <tr key={i}>
               <td>{block.id}</td>
               <td>{block.timestamp}</td>
-              <td>{block.actions}</td>
+              <td>{block.actionsCount}</td>
               <td>{block.block_num}</td>
             </tr>
           ))
