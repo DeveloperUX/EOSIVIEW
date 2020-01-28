1d37e4ad4b8e74b92c1c7cb44d6c701028686272
Moustafa Ismael <mismael7@gmail.com>
2020-01-28 18:40:45 +0200

    move the getActionsCount function into its own file

--- src/App.js
+++ src/App.js
@@ -5,25 +5,7 @@ import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 import ProgressButton from 'react-progress-button';
 import JsonTag from 'react-json-pretty';
-
-const getActionsCount = (block) => {
-  let actionsCount = 0;
-  // make sure we default this to an empty array in case it's undefined
-  const { transactions = [] } = block;
-  for (let transaction of transactions) {
-    const { trx = {} } = transaction;
-    // note: some trx are string IDs while some are full objects
-    if (typeof trx === 'object') {
-      // note: Assuming we need to count both actions and context_free_actions
-      // which are quote: "actions that do not depend upon the blockchain state to perform validation"
-      // temp1.transactions[4].trx.transaction.actions
-      const { transaction = {} } = trx;
-      const { context_free_actions = [], actions = [] } = transaction;
-      actionsCount += context_free_actions.length + actions.length;
-    }
-  }
-  return actionsCount;
-};
+import { getActionsCount } from './helpers';
 
 const Expandable = ({block}) => {
   const [expandRow, setExpandRow] = useState(false);
@@ -38,7 +20,7 @@ const Expandable = ({block}) => {
       {
         expandRow && (
           <tr className={style.detailPane}>
-            <td colSpan={3}>
+            <td colSpan={3} className={style.jsonWrapper}>
               {/*<JsonTag src={block} collapseStringsAfterLength={30} />*/}
               <JsonTag data={block} mainStyle="white-space:pre-wrap;word-break:break-word" />
             </td>
